f4ea31d3cbf4232a4fab6d17a3ae0cfeb755b416
BbinMaharjan <bbinmaharjan@gmail.com>
2021-08-05 10:20:59 +0545

    COM#2:Calculating Net Salary by Tax Bracket

--- net-salary-calculator.js
+++ net-salary-calculator.js
@@ -1,36 +1,36 @@
 // Declare an employees array
 const employees = [
-  { name: "John Doe", salary: 9500 },
-  { name: "Jane Doe", salary: 16500 },
-  { name: "Jim Doe", salary: 24999 },
-  { name: "Josh Doe", salary: 37000 },
+  { name: "John Doe", grossSalary: 9500 },
+  { name: "Jane Doe", grossSalary: 16500 },
+  { name: "Jim Doe", grossSalary: 24999 },
+  { name: "Josh Doe", grossSalary: 37000 },
 ];
 
 //Calculating Net Salary
-const calculateNetSalary = (salary) => {
+const calculateNetSalary = (grossSalary) => {
   let netSalary;
   switch (true) {
-    case salary <= 10000: //Tax bracket <=10000 by 2% Tax
-      netSalary = salary - salary * 0.02;
+    case grossSalary <= 10000: //Tax bracket <=10000 by 2% Tax
+      netSalary = grossSalary - grossSalary * 0.02;
       break;
-    case salary > 10000 && salary <= 20000: //Tax bracket >10000 to <=20000 by 4% Tax
-      netSalary = salary - (10000 * 0.02 + (salary - 10000) * 0.04);
+    case grossSalary > 10000 && grossSalary <= 20000: //Tax bracket >10000 to <=20000 by 4% Tax
+      netSalary = grossSalary - (10000 * 0.02 + (grossSalary - 10000) * 0.04);
       break;
-    case salary > 20000 && salary <= 30000: //Tax bracket >20000 to <=30000 by 7% Tax
-      netSalary = salary - (20000 * 0.02 + (salary - 20000) * 0.07);
+    case grossSalary > 20000 && grossSalary <= 30000: //Tax bracket >20000 to <=30000 by 7% Tax
+      netSalary = grossSalary - (20000 * 0.02 + (grossSalary - 20000) * 0.07);
       break;
-    case salary >= 30001: //Tax bracket >30000 by 10% Tax
-      netSalary = salary - (30000 * 0.07 + (salary - 30000) * 0.1);
+    case grossSalary >= 30001: //Tax bracket >30000 by 10% Tax
+      netSalary = grossSalary - (30000 * 0.07 + (grossSalary - 30000) * 0.1);
       break;
   }
   return netSalary;
 };
 
-// Map the employees array and output their net salary by calling calculateNetSalary function
+// Map the employees array and output their Net Salary by calling calculateNetSalary function
 employees.map((emp) => {
   console.log(
     `Name:  ${emp.name} 
-    Gross Salary: ${emp.salary}  
-    NetSalary: ${calculateNetSalary(emp.salary)}`
+    Gross Salary: ${emp.grossSalary}  
+    Net Salary: ${calculateNetSalary(emp.grossSalary)}`
   );
 });
