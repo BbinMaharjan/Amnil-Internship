ac683a6313fdd88e87f8ed16c22116f3ebcddbff
BbinMaharjan <bbinmaharjan@gmail.com>
2021-08-10 21:02:37 +0545

    COM#3 -  Returns cube of a number

--- javascript-exercises-1.js
+++ javascript-exercises-1.js
@@ -1,6 +1,6 @@
 //JavaScript Exercies
 
-//Ex:1 Start
+//Ex:1 Start (Palindrome word or not)
 String.prototype.isPalindrome = function (word) {
   const arrayValues = string.split(""); //converting strinf to an array
 
@@ -19,3 +19,13 @@ String.prototype.isPalindrome((string = "shovel"));
 String.prototype.isPalindrome((string = "noon"));
 String.prototype.isPalindrome((string = "youtube"));
 //Ex:1 End
+
+//Ex:2 Start (Cube of a number)
+Number.prototype.cubeNumber = function (num) {
+  const cube = num ** 3;
+  console.log(`${num} cube is ${cube}`);
+  return cube;
+};
+let num = 2;
+Number.prototype.cubeNumber(num);
+//Ex:2 End
